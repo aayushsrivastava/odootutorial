44e3227df116eb738b990d2d8d22b68aec12b64b
Aayush Srivastava <37486345+aayushsrivastava@users.noreply.github.com>
2019-06-26 11:18:58 +0530

    Distinction Many2one

--- models/gmcmodule_employeetable.py
+++ models/gmcmodule_employeetable.py
@@ -6,7 +6,7 @@ class GmcModuleEmployeeTable(models.Model):
 	cardno = fields.Char('Card No.', required=True)
 	name = fields.Char('Name', required=True)
 	dateofjoining = fields.Date('Date of Joining', required=True)
-	policytype_id = fields.Char('Policy Type', required=True)
+	policytype_id = fields.Many2one('gmcmodule.policytable', ondelete='set null', string='Policy Type', index=True)
 	suminsured = fields.Float('Sum Insured(in Lac)', required=True)	
 	proratapremium = fields.Float('Prorata Premium in Rs.', required=True)
 	coveringdays = fields.Integer('Covering Days', required=True)
